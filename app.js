fabedb8201dceb8eb508f199e18fd2fd6b415a7c
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2012-09-06 17:28:50 -0300

    Bugfix no router.

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ app.configure(function(){
 	app.set('views', __dirname + '/views');
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
-	app.use(express.cookieParser());
-	app.use(express.session({secret: 'exatec-nodejs'}));
 	app.use(app.router);
 	app.use(express.static(__dirname + '/public'));
 	app.use(express.errorHandler());
